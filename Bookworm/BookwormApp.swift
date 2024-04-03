5cf81e74e2301eb480824eedb9af63dd1395829b
Vinicius Ledis <161168846+viniciusledis@users.noreply.github.com>
2024-04-03 08:59:12 -0300

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Vinicius Ledis on 03/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
